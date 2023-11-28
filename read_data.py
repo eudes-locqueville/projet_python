39884bd6737fb84550b489c48d200fbf91ef7302
Julien L <julien.leclerc@hec.edu>
2023-11-28 14:26:09 +0100

    commit comprendre colonnes

--- read_data.py
+++ read_data.py
@@ -1,20 +1,6 @@
 import requests
 import pandas as pd
 '''
-url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-france/api-docs.json"
-
-response = requests.get(url)
-
-if response.status_code == 100:
-    # La requête a réussi
-    data = response.json()  # Convertir la réponse JSON en objet Python
-    print(data)
-else:
-    # La requête a échoué
-    print(f"Erreur {response.status_code}: {response.text}")
-'''
-import requests
-
 # URL de l'API endpoint pour récupérer la liste des colonnes
 api_url = "https://koumoul.com/data-fair/api/v1/datasets/dpe-france/safe-schema"
 
@@ -40,29 +26,32 @@ else:
 
 import requests
 
-# Nom de la colonne que vous souhaitez explorer
-column_name = "tr001_modele_dpe_type_libelle"
+colonnes_pertinentes = ["consommation_energie", "classe_consommation_energie", "estimation_ges", "classe_estimation_ges", "annee_construction", "tr002_type_batiment_description", "code_insee_commune_actualise", "geo_adresse"]
 
-# URL de l'API endpoint pour récupérer les valeurs distinctes de la colonne
-api_url = f"https://koumoul.com/data-fair/api/v1/datasets/dpe-france/values/{column_name}"
+def obtenir_valeurs_distinctes(liste):
+    for column_name in liste:
+        # URL de l'API endpoint pour récupérer les valeurs distinctes de la colonne
+        api_url = f"https://koumoul.com/data-fair/api/v1/datasets/dpe-france/values/{column_name}"
 
-# Paramètres de requête (sans filtre)
-params = {"limit": 10}
+        # Paramètres de requête (sans filtre)
+        params = {"limit": 10}
 
-# Faire une requête GET à l'API
-response = requests.get(api_url, params=params)
+        # Faire une requête GET à l'API
+        response = requests.get(api_url, params=params)
 
-# Vérifier si la requête a réussi (code de statut HTTP 200)
-if response.status_code == 200:
-    # Convertir la réponse JSON en une liste Python
-    values_data = response.json()
+        # Vérifier si la requête a réussi (code de statut HTTP 200)
+        if response.status_code == 200:
+            # Convertir la réponse JSON en une liste Python
+            values_data = response.json()
 
-    # Afficher les valeurs distinctes
-    print(f"Valeurs distinctes pour la colonne '{column_name}':")
-    for value in values_data:
-        print(value)
+            # Afficher les valeurs distinctes
+            print(f"Valeurs distinctes pour la colonne '{column_name}':")
+            for value in values_data:
+                print(value)
 
-else:
-    # Afficher un message d'erreur si la requête a échoué
-    print(f"Erreur {response.status_code}: {response.text}")
-'''
+        else:
+            # Afficher un message d'erreur si la requête a échoué
+            print(f"Erreur {response.status_code}: {response.text}")
+
+# Appeler la fonction avec la liste de colonnes pertinentes
+obtenir_valeurs_distinctes(colonnes_pertinentes)
