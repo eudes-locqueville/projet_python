2099106cf174efd418cf5be96ca5b5b8daaaa6f8
Julien L <julien.leclerc@hec.edu>
2023-12-08 17:14:36 +0100

    réussite0812

--- comprendre_api/Efficace.py
+++ comprendre_api/Efficace.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-# Définissez vos contraintes et les colonnes pertinentes
+# Définissez vos contraintes, les colonnes pertinentes, et les filtres
 params_lines = {
     "page": 1,
     "size": 20,
@@ -10,7 +10,9 @@ params_lines = {
         "estimation_ges", "classe_estimation_ges",
         "annee_construction", "tr002_type_batiment_description",
         "code_insee_commune_actualise", "geo_adresse"
-    ])
+    ]),
+    "q": "code_insee_commune_actualise:01430",
+    # Si nécessaire, utilisez le paramètre "qs" pour des requêtes plus complexes
 }
 
 url_lines = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-france/lines"
@@ -38,11 +40,10 @@ if response_lines.status_code == 200:
     ])
 
     # Enregistrez le DataFrame au format Excel avec le nom de fichier 'donnees_associées.xlsx'
-    df.to_excel('test_lino_2.xlsx', index=False)
+    df.to_excel('donnees_associées.xlsx', index=False)
 
     # Affichez le DataFrame final
     print(df)
 
 else:
     print(f"Erreur {response_lines.status_code}: {response_lines.text}")
-
