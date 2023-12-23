7ebf8c08cc0d3d3627eaa75a89ece11dfaf28cc4
Julien L <julien.leclerc@hec.edu>
2023-12-23 16:36:01 +0100

    généralisation fonction d'extraction

--- comprendre_api/Extracteur_Donnees.py
+++ comprendre_api/Extracteur_Donnees.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-def liste_donnees(code_commune):
-    # Définissez vos contraintes, les colonnes pertinentes, et les filtres
+def liste_donnees(code_commune=None):
+    # liste_donnees prend par défaut aucune condition donc on pioche dans la France, on peut aussi imposer une condition de code postal
     params_lines = {
         "page": 1,
         "size": 3000,
@@ -12,8 +12,7 @@ def liste_donnees(code_commune):
             "annee_construction", "tr002_type_batiment_description",
             "code_insee_commune_actualise", "geo_adresse"
         ]),
-        "q": f"code_insee_commune_actualise:{code_commune}",
-        # Si nécessaire, utilisez le paramètre "qs" pour des requêtes plus complexes
+        "q": f"code_insee_commune_actualise:{code_commune}" if code_commune else None,
     }
 
     url_lines = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-france/lines"
@@ -42,14 +41,11 @@ def liste_donnees(code_commune):
         ])
 
         # Enregistrez le DataFrame au format Excel avec le nom de fichier 'donnees_associées.xlsx'
-        df.to_excel(f'liste_finale_{code_commune}.xlsx', index=False)
+        #filename = f'liste_finale_{code_commune}.xlsx' if code_commune else 'liste_finale_France.xlsx'
+        #df.to_excel(filename, index=False)
 
-        # Affichez le DataFrame final
         return df
 
     else:
         return f"Erreur {response_lines.status_code}: {response_lines.text}"
 
-# Exemple d'utilisation avec le code commune 91477
-result_df = liste_donnees("91477")
-print(result_df)
