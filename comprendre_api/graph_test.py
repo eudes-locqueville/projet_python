ad516b8a0625edeea6d489472704db1202d46d62
Julien L <julien.leclerc@hec.edu>
2023-12-28 17:49:29 +0100

    nouveaux_graph

--- comprendre_api/graph_test.py
+++ comprendre_api/graph_test.py
@@ -2,12 +2,28 @@ import plotly.express as px
 import pandas as pd
 from Clean_donnees import liste_propre
 import streamlit as st
+import plotly.graph_objs as go
+
+def filter_data_by_year(data, year):
+    datac = data.copy()
+    if year is not None:
+        # Filtrer les données en fonction de l'année fournie
+        if year <= 1945:
+            datac = datac[datac['annee_construction'] <= 1945]
+        elif (year > 1945) & (year <= 1975) :
+            datac = datac[(datac['annee_construction'] > 1945) & (datac['annee_construction'] <= 1975)]
+        elif (year > 1975) & (year <= 2000):
+            datac = datac[(datac['annee_construction'] > 1975) & (datac['annee_construction'] <= 2000)]
+        elif year > 2000:
+            datac = datac[datac['annee_construction'] > 2000]
+
+    return datac
 
 def testgraph(code_commune=None, taille_echantillon=3000):
     data = liste_propre(code_commune, taille_echantillon)
     value_counts = data['classe_consommation_energie'].value_counts().reset_index()
     value_counts.columns = ['classe_consommation_energie', 'Count']
-    
+
     fig = px.bar(value_counts, 
                  x='classe_consommation_energie', 
                  y='Count', 
@@ -17,9 +33,36 @@ def testgraph(code_commune=None, taille_echantillon=3000):
                  opacity=0.8,
                  width=800,
                  height=500)
-    
-    # Afficher la figure dans Streamlit
-    st.plotly_chart(fig)
 
-# Test de la fonction
-testgraph()
+    # Convertir la figure en instance de plotly.graph_objs.Figure
+    return go.Figure(fig)
+
+
+
+
+# Nouvelle fonction pour la visualisation par année de construction
+def par_annee(code_commune=None, taille_echantillon=3000, year=None):
+    data = liste_propre(code_commune, taille_echantillon)
+
+    # Filtrer les données en fonction de l'année de construction
+    data = filter_data_by_year(data, year)
+
+    # Créer un graphique à barres
+    value_counts = data['classe_consommation_energie'].value_counts().reset_index()
+    value_counts.columns = ['classe_consommation_energie', 'Count']
+
+    fig = px.bar(value_counts, 
+                 x='classe_consommation_energie', 
+                 y='Count', 
+                 title=f'Value Counts of classe_consommation_energie - Year: {year}',
+                 template='plotly_dark',
+                 color_discrete_sequence=['#F63366'],
+                 opacity=0.8,
+                 width=800,
+                 height=500)
+
+    # Convertir la figure en instance de plotly.graph_objs.Figure
+    return go.Figure(fig)
+
+
+    
--- comprendre_api/interface.py
+++ comprendre_api/interface.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 import joblib
 import plotly.express as px
 from Clean_donnees import liste_propre
 from regression import predict_dpe
-from graph_test import testgraph
+from graph_test import testgraph, par_annee, filter_data_by_year
 
 # Charger le scaler préalablement entraîné
 scaler_path = "scaler.pkl"
 scaler = joblib.load(scaler_path)
 
-
 # Interface Streamlit
 def main():
-    st.title("Prédiction DPE")
+    st.title("Prédiction de la classe de consommation d'énergie de votre logement et comparaison par rapport aux autres biens immobiliers")
 
     # Saisie utilisateur
     surface = st.slider("Surface du logement (m²)", 10, 300, 100)
-    consommation_energie = st.slider("Consommation énergétique annuelle", 50, 1000, 500)
+    consommation_energie = st.slider("Consommation énergétique annuelle en kilo Watt heure d'énergie primaire, par m² et par an", 50, 1000, 500)
+    year = st.text_input("Entrez l'année de construction (facultatif):")
 
     # Entrée pour le code commune
     code_commune = st.text_input("Votre code commune", "")
@@ -32,12 +31,22 @@ def main():
         # Afficher la prédiction
         st.success(f"Votre lettre DPE prédite est : {prediction}")
 
-        # Afficher les graphiques côte à côte avec des titres
-        st.subheader("Graphique avec votre code commune")
-        st.plotly_chart(testgraph(code_commune=code_commune))
-
+        # Afficher le graphique global en France
         st.subheader("Graphique France")
-        st.plotly_chart(testgraph())
+        fig_france = testgraph(code_commune=code_commune)  # Utiliser testgraph avec ou sans code commune
+        st.plotly_chart(fig_france)
+
+        # Afficher le graphique en fonction du département
+        if code_commune:
+            st.subheader(f"Comparez votre bien à ceux de votre commune {code_commune}")
+            fig_departement = testgraph(code_commune=code_commune)
+            st.plotly_chart(fig_departement)
+
+        # Afficher les graphiques par année de construction
+        if year:
+            st.subheader(f"Comparez votre bien à ceux construits à la même époque ({year})")
+            fig_annee = par_annee(year=year)
+            st.plotly_chart(fig_annee)
 
 # Lancer l'application
 if __name__ == "__main__":
