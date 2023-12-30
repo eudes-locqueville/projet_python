5c1593690666882a1de7db9926dda166ef3c707a
Your Name <you@example.com>
2023-12-30 20:34:33 +0100

    sorting files

--- details/regression.py
+++ details/regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from Clean_donnees import liste_propre
+from clean_donnees import liste_propre
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
